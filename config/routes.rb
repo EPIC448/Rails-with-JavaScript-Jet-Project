d502b5cdf9d847484d429be744f6bda7b462eede
epic0448 <samuelogundiran@Yahoo.com>
2018-12-12 08:06:56 -0500

    Refactory

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#new'
   get 'flights/index'
-  # get '/signin' => "sessions#login"
-  get '/login'  => "session#new"
+  get '/signin' => "sessions#login"
+  # get '/login'  => "session#new"
   post '/sessions' => "sessions#create"
   delete '/logout' => "sessions#destroy"
 
